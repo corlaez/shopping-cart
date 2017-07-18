fc45f49fe417874dcab6217a2d01fb96cebec36f
J Armando Cordova <armanpela@gmail.com>
2017-07-18 10:24:10 -0500

    payment reaches server correctly

--- website/src/main/java/com/cesarmando/website/forms/PaymentData.java
+++ website/src/main/java/com/cesarmando/website/forms/PaymentData.java
@@ -1,9 +1,11 @@
 package com.cesarmando.website.forms;
 
+import com.cesarmando.website.dao.model.ProductE;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by jarma on 7/10/2017.
@@ -17,5 +19,5 @@ public class PaymentData {
     private String ccv;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date expirationDate;
-    private String items;
+    private List<ProductE> items;
 }
